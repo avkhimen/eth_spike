6168410f249445c06eaebbedcd5d1b3f12147e1b
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-11-13 01:39:09 -0700

    Getting spike labels

--- app/main.py
+++ app/main.py
@@ -4,4 +4,33 @@ import numpy as np
 df = pd.read_csv('data/original_files/ETHXBT_60.csv', header=None,
                  names=['unix_timestamp','open_price','high_price','low_price','close_price','other_1','other_2'])
 
+df['unix_timestamp'] = df['unix_timestamp'].astype(int)
+
+df['timestamp'] = pd.to_datetime(df['unix_timestamp'], unit='s')
+
+df = df.drop(['unix_timestamp', 'other_1', 'other_2'], axis=1)
+
+df = df[['timestamp','open_price','high_price','low_price','close_price']]
+
+df = df.resample('4H', on='timestamp').agg({
+    'open_price': 'first',
+    'high_price': 'max',
+    'low_price': 'min',
+    'close_price': 'last'
+    })
+
+df = df[['close_price']]
+
+df = df.pct_change()
+
+df = df[1:]
+
+df['close_price_perc'] = df['close_price'] * 100
+
+threshold = 4
+
+df['spike'] = (df['close_price_perc'] > threshold).fillna(False)
+
+df = df[['close_price','spike']]
+
 print(df)
