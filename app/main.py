261d0bfaf822ef3960978dd3248a4e2da01180c5
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-11-13 23:09:06 -0700

    Creating model

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
+import warnings
+from sklearn import svm
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+warnings.simplefilter('ignore')
 
 df = pd.read_csv('data/original_files/ETHXBT_60.csv', header=None,
                  names=['unix_timestamp','open_price','high_price','low_price','close_price','other_1','other_2'])
+
+df = df.dropna()
 
 df['unix_timestamp'] = df['unix_timestamp'].astype(int)
 
@@ -22,11 +31,11 @@ df = df.resample('4H', on='timestamp').agg({
 
 df = df[['close_price']]
 
-df = df.pct_change()
+df['close_price_perc'] = df['close_price'].pct_change()
 
 df = df[1:]
 
-df['close_price_perc'] = df['close_price'] * 100
+df['close_price_perc'] = df['close_price_perc'] * 100
 
 threshold = 4
 
@@ -34,10 +43,32 @@ df['spike'] = (df['close_price_perc'] > threshold).fillna(False)
 
 df = df[['close_price','spike']]
 
-close_prices_list = sliding_window_view(df['close_price'], 4).tolist()
+offset = 4
 
-spike_list = sliding_window_view(df['spike'], 1)[3:].tolist()
+close_prices_list = sliding_window_view(df['close_price'], offset).tolist()
 
-print(df)
-#print(close_prices_list, len(close_prices_list))
-#print(spike_list, len(spike_list))
+spike_list = sliding_window_view(df['spike'], 1).tolist()[offset-1:]
+spike_list = [elem[0] for elem in spike_list]
+
+X = close_prices_list[:-1]
+y_ = spike_list[1:]
+
+y = []
+for elem in y_:
+    y.append(str(elem))
+
+print(df.head(10))
+print(X[:5], len(X))
+print(y[:5], len(y))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+clf = svm.SVC()
+clf = tree.DecisionTreeClassifier()
+
+clf.fit(X_train, y_train)
+
+yhat = clf.predict(X_test)
+
+acc = accuracy_score(y_test, yhat)
+print('Accuracy: %.3f' % acc)
